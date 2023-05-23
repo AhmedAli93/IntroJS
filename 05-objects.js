02e5c5da41b27316c6779a4f6dc08f8183f66b83
Ahmed Ali <ahmedali@Ahmeds-MacBook-Pro.local>
2023-05-23 16:49:36 -0700

    Conditions & Loops

--- 05-objects.js
+++ 05-objects.js
@@ -1,10 +1,20 @@
 const cookie = {
     name: "Choclate Chip",
     isGlutenFree: false,
-    
+
 
 };
 
 cookie.name = "Chip choclate"
 cookie.isGlutenFree = true
 console.log(cookie);
+
+class Cookie{
+    constructor(name, isGlutenFree){
+        this.name = name;
+        this.isGlutenFree = isGlutenFree;
+    }
+}
+const myCookie = new Cookie("Iced Oatmeal", false);
+
+console.log(myCookie);
---
+++ 06-if-else.js
@@ -0,0 +1,9 @@
+let attendence = 14;
+
+if(attendence < 15){
+    console.log("We have room");
+} else if (attendence > 15) {
+    console.log("We're over capacity");
+} else{
+    console.log("We're at full capacity");
+}
